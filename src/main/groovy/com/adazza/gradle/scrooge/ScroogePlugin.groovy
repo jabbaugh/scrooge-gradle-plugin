0be1cdefbf598681d58a435c168db15df5e8ff09
Ben McNiel <ben@adazza.com>
2017-02-24 01:21:38 -0500

    Initial commit for scrooge plugin.

---
+++ src/main/groovy/com/adazza/gradle/scrooge/ScroogePlugin.groovy
@@ -0,0 +1,12 @@
+package com.adazza.gradle.scrooge
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class ScroogePlugin implements Plugin<Project> {
+
+    @Override
+    void apply(final Project project) {
+        project.tasks.add("compileScrooge", ScroogeCompileTask)
+    }
+}
