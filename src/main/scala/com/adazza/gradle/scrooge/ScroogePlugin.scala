fa308b6c15a6367894a1093b182a388aea930154
Daniel Barajas <daniel@adazza.com>
2017-06-08 14:41:27 -0700

    Remove unnecessary comment in ScroogePlugin.scala

--- src/main/scala/com/adazza/gradle/scrooge/ScroogePlugin.scala
+++ src/main/scala/com/adazza/gradle/scrooge/ScroogePlugin.scala
@@ -3,10 +3,6 @@ package com.adazza.gradle.scrooge
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 
-/**
-  * Created by danielbarajas on 6/8/17.
-  */
-
 class ScroogePlugin extends Plugin[Project] {
 
   def apply(project: Project): Unit = {
